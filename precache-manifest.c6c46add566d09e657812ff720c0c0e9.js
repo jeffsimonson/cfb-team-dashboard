1110cf7db79dbcfe54aaeeee0448eed29d7270af
Jeff Simonson <jeffsimonson@live.com>
2020-10-19 03:27:34 -0500

    Updates

---
+++ precache-manifest.c6c46add566d09e657812ff720c0c0e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72498e650286b4c830fd6d6320450217",
+    "url": "/cfb-team-dashboard/index.html"
+  },
+  {
+    "revision": "d35bd6a5c298d559753d",
+    "url": "/cfb-team-dashboard/static/css/main.d6713f08.chunk.css"
+  },
+  {
+    "revision": "476ca3354bd26bec707a",
+    "url": "/cfb-team-dashboard/static/js/2.af1445e0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/cfb-team-dashboard/static/js/2.af1445e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d35bd6a5c298d559753d",
+    "url": "/cfb-team-dashboard/static/js/main.bbe4d574.chunk.js"
+  },
+  {
+    "revision": "a4b47c6ab4f5f8fc3644",
+    "url": "/cfb-team-dashboard/static/js/runtime-main.d5e7a3bc.js"
+  }
+]);
